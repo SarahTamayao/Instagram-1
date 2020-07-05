fbdccbdde989ef589d87369b5b4de5168a85202c
Xurxo Riesco <xurxor@fb.com>
2020-07-05 02:14:44 -0400

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Xurxo Riesco on 7/5/20.
+//  Copyright © 2020 Xurxo Riesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
